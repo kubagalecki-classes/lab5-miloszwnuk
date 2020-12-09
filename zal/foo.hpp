0c06e19e817758c73ec8be3805491695d5d0ad61
miloszwnuk <01157070@pw.edu.pl>
2020-12-09 18:43:46 +0000

    yes

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,21 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+   vector< char > likesAnimals;
+    list< Human >::reverse_iterator it;
+    for (it = people.rbegin(); it != people.rend(); it++)
+    {
+        it->birthday();
+
+        if (it->isMonster() == true)
+        {
+            likesAnimals.push_back('n');
+        }
+        else
+        {
+            likesAnimals.push_back('y');
+        }
+    }
+    return  { likesAnimals };   
+    
 }
